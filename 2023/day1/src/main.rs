c1819874dfd1698b0775ab750111afbf85d0f223
Ahmed Abdelaziz <Ahmed.Abdelaziz.GM@gmail.com>
2023-12-06 00:42:46 +0200

    Day 1: Both Parts Completed

---
+++ 2023/day1/src/main.rs
@@ -0,0 +1,38 @@
+use std::io::stdin;
+use std::io::prelude::*;
+
+fn main() {
+    let mut ans = 0;
+    let mut last = 0;
+    let nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    for line in stdin().lock().lines() {
+        let s = line.unwrap();
+        let mut is_first = true;
+
+        for (i, ch) in s.chars().enumerate() {
+            if ch.is_digit(10) {
+                last = ch.to_digit(10).unwrap();
+                if is_first {
+                    ans += last * 10;
+                    is_first = false;
+                }
+                continue;
+            };
+            for (j, num) in nums.iter().enumerate() {
+                if i + num.len() <= s.len() && &s[i..i+num.len()] == *num {
+                    last = (j + 1) as u32;
+                    if is_first {
+                        ans += last * 10;
+                        is_first = false;
+                    }
+                    break;
+                }
+            }
+
+        }
+        ans += last;
+    }
+
+    println!("{ans}");
+}
